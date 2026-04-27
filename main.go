381693cc6bb274e7d7ad1f451891e78cce1e0c0a
Adewale <a.adewalemuideen@gmail.com>
2026-04-27 10:30:21 +0100

    deployment prep

--- main.go
+++ main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	http.HandleFunc("/", homeHandler)
 	http.HandleFunc("/ascii-art", asciiHandler)
 	http.HandleFunc("/share", shareHandler)
-	fmt.Println("Server running on port 8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" // fallback for local
+	}
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+
 }
